baf60be733ff430af891100018575e51310162d1
Shyamal Sriniketh <shyamal.sriniketh@gmail.com>
2025-11-11 16:44:58 -0500

    chore: created intent from MainActivity.java to LoginActivity.java

--- SMARTAIR/app/src/main/java/com/example/smartair/LoginActivity.java
+++ SMARTAIR/app/src/main/java/com/example/smartair/LoginActivity.java
@@ -1,15 +1,13 @@
 package com.example.smartair;
 
-import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent i = new Intent(this, LoginActivity.class);
-        startActivity(i);
+        setContentView(R.layout.activity_login);
     }
 }
