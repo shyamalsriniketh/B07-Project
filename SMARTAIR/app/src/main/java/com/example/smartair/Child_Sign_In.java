4fe9498a49dc7fbfd7a9ab6972aa961c8f6b6c94
Shak <ariknon@gmail.com>
2025-11-11 23:15:53 -0500

    Added a working sign up. Can revise more later

---
+++ SMARTAIR/app/src/main/java/com/example/smartair/Child_Sign_In.java
@@ -0,0 +1,62 @@
+package com.example.smartair;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Child_Sign_In extends AppCompatActivity {
+    EditText emailfield;
+    EditText passwordfield;
+    Button saveButton;
+    FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_child_sign_in);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        mAuth = FirebaseAuth.getInstance();
+        emailfield = findViewById(R.id.editTextTextEmailAddress2);
+        passwordfield = findViewById(R.id.editTextTextPassword2);
+        saveButton = findViewById(R.id.button6);
+        saveButton.setOnClickListener(v -> save_button());
+    }
+    private void save_button(){
+        String email = emailfield.getText().toString();
+        String password = passwordfield.getText().toString();
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, task -> {
+            if(task.isSuccessful()){
+                FirebaseUser user = mAuth.getCurrentUser();
+                Toast.makeText(this, "Account created " + user.getEmail(), Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(this, "Sign up failed " +
+                        task.getException().getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+    public void go_back(View v){
+        Intent change_screen = new Intent(this, SignUp.class);
+        startActivity(change_screen);
+    }
+}
