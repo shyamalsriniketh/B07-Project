215e98a4d7a6bdf128f610bedb2d79c43693598f
Shyamal Sriniketh <shyamal.sriniketh@gmail.com>
2025-11-27 13:04:27 -0500

    fix: edited onboarding files to solve bugs and meet requirements

--- SMARTAIR/app/src/main/java/com/example/smartair/ChildOnboardingFragment1.java
+++ SMARTAIR/app/src/main/java/com/example/smartair/ChildOnboardingFragment1.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 public class ChildOnboardingFragment1 extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.child_onboarding, container, false);
--- SMARTAIR/app/src/main/java/com/example/smartair/ParentOnboardingFragment1.java
+++ SMARTAIR/app/src/main/java/com/example/smartair/ParentOnboardingFragment1.java
@@ -9,7 +9,6 @@ import androidx.fragment.app.Fragment;
 
 public class ParentOnboardingFragment1 extends Fragment {
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.parent_onboarding, container, false);
