a72713b1f4e81e373d0f85628d4fad825b98984d
Shyamal Sriniketh <shyamal.sriniketh@gmail.com>
2025-11-29 18:16:12 -0500

    fix: updated child attributes to match requirements

--- SMARTAIR/app/src/main/java/com/example/smartair/ViewChildActivity.java
+++ SMARTAIR/app/src/main/java/com/example/smartair/ViewChildActivity.java
@@ -3,7 +3,6 @@ package com.example.smartair;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
