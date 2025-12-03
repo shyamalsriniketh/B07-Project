dd9d3766902b89d6a4f67bbba76eebe43f81909a
rayyan442 <rayyannaseer04@gmail.com>
2025-12-02 22:55:46 -0500

    fixed provider report

--- SMARTAIR/app/src/main/java/com/example/smartair/ProviderViewReports.java
+++ SMARTAIR/app/src/main/java/com/example/smartair/ProviderViewReports.java
@@ -2,6 +2,7 @@ package com.example.smartair;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -42,16 +43,20 @@ public class ProviderViewReports extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         providerId = mAuth.getCurrentUser().getUid();
         reference = FirebaseDatabase.getInstance().getReference();
-        reference.addValueEventListener(new ValueEventListener() {
+        Log.d("hello", "44");
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 TextView tv = findViewById(R.id.providerReportText);
                 tv.setText("");
                 for (DataSnapshot code : snapshot.child("providers").child(providerId).child("childrenAndCodes").getChildren()) {
+                    Log.d("hello", "33");
                     if (code.getKey().equals(snapshot.child("children").child(code.getValue(String.class)).child("inviteCodeProvider").getValue(String.class))) {
                         //display child's report
+                        Log.d("hello", "123");
                         String childId = code.getValue(String.class);
                         loadChildReport(childId);
+                        Log.d("hello", "1234");
                     }
                     else {
                         reference.child("providers").child(providerId).child("childrenAndCodes").child(code.getKey()).setValue(null);
@@ -62,8 +67,7 @@ public class ProviderViewReports extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError error) {}
         });
-        pieChartZones = findViewById(R.id.pieChartZones);
-        lineChartRescue = findViewById(R.id.lineChartRescue);
+
         Button button = findViewById(R.id.button10);
         Button button2 = findViewById(R.id.button9);
         button.setOnClickListener(v -> {
@@ -76,6 +80,7 @@ public class ProviderViewReports extends AppCompatActivity {
         });
     }
     private void loadChildReport(String childId){
+
         DatabaseReference childRef = reference.child("children").child(childId);
 
 
@@ -90,25 +95,31 @@ public class ProviderViewReports extends AppCompatActivity {
             boolean showControllerSummary = getBool(settingsSnap, "controllerSummary");
             boolean three_months = getBool(settingsSnap, "threeMonths");
             boolean six_months = getBool(settingsSnap, "sixMonths");
+            Log.d("key", " " + showRescue);
+            List<String> lines = new ArrayList<>();
+
             if(showSymptoms){
                 reference.child("logs").child(childId).child("dailyCheckin").addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         for(DataSnapshot item : snapshot.getChildren()){
-                            String Date = item.getKey();
-                            long date = Long.parseLong(Date);
+
+
+                            long date = Long.parseLong(item.getKey());
+
                             long now = System.currentTimeMillis();
-                            long sixMonthsAgo = now - (long)(6L * 30 * 24 * 3600 * 1000);
-                            long threeMonthsAgo = now - (long)(3L * 30 * 24 * 60 * 60 * 1000);
+                            long sixMonthsAgo = now - (6L * 30 * 24 * 3600 * 1000);
+                            long threeMonthsAgo = now - (3L * 30 * 24 * 60 * 60 * 1000);
                             for(DataSnapshot node : item.getChildren()){
                                 String key = node.getKey();
                                 if(key.equals("triggers")) continue;
-                                String value = node.getValue(String.class);
+
+                                String value = String.valueOf(node.getValue());
                                 if(three_months && date >= threeMonthsAgo){
-                                    appendLine(key + "->" + value);
+                                    appendLine(key + "  ->  " + value);
                                 }
                                 else if(six_months && date >= sixMonthsAgo){
-                                    appendLine(key + "->" + value);
+                                    appendLine(key + "  ->  " + value);
                                 }
                             }
                         }
@@ -125,8 +136,8 @@ public class ProviderViewReports extends AppCompatActivity {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         for(DataSnapshot item : snapshot.getChildren()){
-                            String Date = item.getKey();
-                            long date = Long.parseLong(Date);
+
+                            long date = Long.parseLong(item.getKey());
                             long now = System.currentTimeMillis();
                             long sixMonthsAgo = now - (long)(6L * 30 * 24 * 3600 * 1000);
                             long threeMonthsAgo = now - (long)(3L * 30 * 24 * 60 * 60 * 1000);
@@ -134,10 +145,10 @@ public class ProviderViewReports extends AppCompatActivity {
                                 String key = node.getKey();
                                 Object value = node.getValue();
                                 if(three_months && date >= threeMonthsAgo){
-                                    appendLine(key + "->" + value);
+                                    appendLine(key + "  ->  " + value);
                                 }
                                 else if(six_months && date >= sixMonthsAgo){
-                                    appendLine(key + "->" + value);
+                                    appendLine(key + "  ->  " + value);
                                 }
                             }
                         }
@@ -154,8 +165,8 @@ public class ProviderViewReports extends AppCompatActivity {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         for(DataSnapshot item : snapshot.getChildren()){
-                            String Date = item.getKey();
-                            long date = Long.parseLong(Date);
+                            long date = Long.parseLong(item.getKey());
+
                             long now = System.currentTimeMillis();
                             long sixMonthsAgo = now - (long)(6L * 30 * 24 * 3600 * 1000);
                             long threeMonthsAgo = now - (long)(3L * 30 * 24 * 60 * 60 * 1000);
@@ -163,12 +174,12 @@ public class ProviderViewReports extends AppCompatActivity {
                                 if(three_months && date >= threeMonthsAgo){
                                     String key = node.getKey();
                                     Object value = node.getValue();
-                                    appendLine(key + "->" + value);
+                                    appendLine(key + "  ->  " + value);
                                 }
                                 else if(six_months && date >= sixMonthsAgo){
                                     String key = node.getKey();
                                     Object value = node.getValue();
-                                    appendLine(key + "->" + value);
+                                    appendLine(key + "  ->  " + value);
                                 }
                             }
                         }
@@ -181,28 +192,27 @@ public class ProviderViewReports extends AppCompatActivity {
                 });
             }
             if(showTriggers){
+                Log.d("hello", "10");
                 reference.child("logs").child(childId).child("dailyCheckin").addListenerForSingleValueEvent(new ValueEventListener(){
+
                     @Override
-                    public void onDataChange(DataSnapshot s){
-                        for(DataSnapshot item : s.getChildren()){
-                            String Date = item.getKey();
-                            long date = Long.parseLong(Date);
+                    public void onDataChange(@NonNull DataSnapshot snapshot){
+                        Log.d("hello", "9");
+                        for(DataSnapshot item : snapshot.getChildren()){
+
+                            long date = Long.parseLong(item.getKey());
                             long now = System.currentTimeMillis();
+                            Log.d("hello", "3");
                             long sixMonthsAgo = now - (long)(6L * 30 * 24 * 3600 * 1000);
                             long threeMonthsAgo = now - (long)(3L * 30 * 24 * 60 * 60 * 1000);
 
-                            if(date >= threeMonthsAgo && three_months){
-                                for(DataSnapshot node : item.getChildren()){
-                                    if(!(node.getKey().equals("triggers"))) continue;
-                                    String display = node.getValue(String.class);
-                                    appendLine(display);
-                                }
-                            }
-                            else if(date >= sixMonthsAgo && six_months){
-                                for(DataSnapshot node : item.getChildren()){
-                                    if(!(node.getKey().equals("triggers"))) continue;
-                                    String display = node.getValue(String.class);
-                                    appendLine(display);
+                            for(DataSnapshot node : item.getChildren()){
+                                if(!(node.getKey().equals("triggers"))) continue;
+                                Log.d("hello", "5");
+                                for(DataSnapshot trig_child : node.getChildren()){
+                                    Object val = trig_child.getValue();
+                                    Log.d("hello", "6");
+                                    appendLine(val.toString());
                                 }
                             }
 
@@ -219,20 +229,21 @@ public class ProviderViewReports extends AppCompatActivity {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         for(DataSnapshot dates : snapshot.getChildren()){
-                            String Date = dates.getKey();
-                            long date = Long.parseLong(Date);
+
+                            long date = Long.parseLong(dates.getKey());
                             long now = System.currentTimeMillis();
                             long sixMonthsAgo = now - (long)(6L * 30 * 24 * 3600 * 1000);
                             long threeMonthsAgo = now - (long)(3L * 30 * 24 * 60 * 60 * 1000);
                             for(DataSnapshot node : dates.getChildren()){
+
                                 if(three_months && (date >= threeMonthsAgo)) {
                                     String key = node.getKey();
-                                    String value = node.getValue(String.class);
+                                    String value = String.valueOf(node.getValue());
                                     appendLine(key + "->" + value);
                                 }
                                 else if(six_months && (date >= sixMonthsAgo)){
                                     String key = node.getKey();
-                                    String value = node.getValue(String.class);
+                                    String value = String.valueOf(node.getValue());
                                     appendLine(key + "->" + value);
                                 }
                             }
@@ -250,41 +261,10 @@ public class ProviderViewReports extends AppCompatActivity {
         });
     }
 
-    private void showZonePieChart(Long green, Long yellow, Long red) {
-
-        List<PieEntry> entries = new ArrayList<>();
-
-        if (green != null && green > 0) entries.add(new PieEntry(green, "Green"));
-        if (yellow != null && yellow > 0) entries.add(new PieEntry(yellow, "Yellow"));
-        if (red != null && red > 0) entries.add(new PieEntry(red, "Red"));
-
-        PieDataSet dataSet = new PieDataSet(entries, "Zone Distribution");
-        PieData data = new PieData(dataSet);
-
-        pieChartZones.setData(data);
-        pieChartZones.getDescription().setEnabled(false);
-        pieChartZones.invalidate();
-    }
-    private void showRescueLineChart(Long rescue) {
-
-        List<Entry> entries = new ArrayList<>();
-
-
-        entries.add(new Entry(0, rescue != null ? rescue : 0));
-        entries.add(new Entry(1, rescue != null ? rescue + 1 : 1));
-        entries.add(new Entry(2, rescue != null ? rescue - 1 : 0));
-
-        LineDataSet dataSet = new LineDataSet(entries, "Rescue Frequency");
-        LineData data = new LineData(dataSet);
-
-        lineChartRescue.setData(data);
-        lineChartRescue.getDescription().setEnabled(false);
-        lineChartRescue.invalidate();
-    }
-
-
     private boolean getBool(DataSnapshot snap, String key) {
+
         Boolean val = snap.child(key).getValue(Boolean.class);
+        Log.d("hello", "100");
         return val != null && val;
     }
     private void appendLine(String text) {
